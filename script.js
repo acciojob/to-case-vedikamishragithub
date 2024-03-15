62544ea84fb0dff74b403852310f2669df2fe781
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-15 22:26:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+
 function toCase(text) {
-  // write your code here
-}
-
-// DO not change the code below
-
-const text = prompt("Enter text:");
-alert(toCase(text));
+	// write your code here
+	return text.toLowerCase() + "-" + text.toUpperCase();
+	 
+	  
+  }
+  
+  // DO not change the code below
+  
+  const text = prompt("Enter text:");
+  alert(toCase(text));
